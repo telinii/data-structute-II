fdcc99dc500963a62b530a036a10c6fa925f8e6a
telinii <telinii@users.noreply.github.com>
2026-08-12 20:27:29 -0300

    pass, comps, trocas em bubble sort c/ obj

--- 06-bubble-sort-obj.mjs
+++ 06-bubble-sort-obj.mjs
@@ -1,14 +1,17 @@
 import {objMotoristas} from "./data/E.D.UniFacef.2026.2.CC.SI/data/motoristas-obj-desord.mjs"
-
 function bubbleSort(vetor, fnComp){
+    pass = 0, comps = 0, trocas = 0
     let trocou
     do{
+        pass++
         trocou = false;
 
         for(let i = 0;i < vetor.length - 1;i++){
+            comps++
             if(fnComp(vetor[i], vetor[i+1])){
                 [vetor[i], vetor[i+1]] = [vetor[i+1], vetor[i]];
                 trocou = true;
+                trocas++
             }
         }
     } while(trocou)
@@ -17,3 +20,4 @@ function bubbleSort(vetor, fnComp){
 bubbleSort(objMotoristas, (elem1, elem2) => elem1.nome_motorista > elem2.nome_motorista)
 
 console.log(objMotoristas)
+console.log(pass, comp, trocas)
